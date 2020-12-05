e409f3e191b79dbeb5d926a55260ba81c317541c
Junaid Najeeb <junaid@adobe.com>
2020-12-05 00:36:06 -0800

    added some swagger docs

--- src/main/java/com/quartz/scheduler/controller/HealthController.java
+++ src/main/java/com/quartz/scheduler/controller/HealthController.java
@@ -1,16 +1,21 @@
 package com.quartz.scheduler.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.quartz.scheduler.model.Health;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(path = "/health")
+@Api(value = "Health Controller - Returns health of the system", produces = MediaType.APPLICATION_JSON_VALUE )
 public class HealthController {
 
-  @RequestMapping(method = RequestMethod.GET)
+  @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  @ApiOperation(value = "Returns health of the system ", response = Health.class)
   public ResponseEntity<Health> getHealth() {
     Health health = Health.create("good");
     return ResponseEntity.ok(health);
